809c63e8b62eacb6926838184f4845c7d55942a1
NandyalaSrinivas <61131782+NandyalaSrinivas@users.noreply.github.com>
2020-12-03 07:45:19 +0530

    Update urls.py

--- StudentProject/StudentProject/urls.py
+++ StudentProject/StudentProject/urls.py
@@ -4,12 +4,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from appstudent import views
 
+app_name = "appstudent"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
     path('application', views.application, name='application'),
     path('candidatelist', views.candidatelist, name='candidatelist'),
+    path('departments', views.departments, name='departments'),
+    path('department/department_wise', views.department_wise, name='department_wise'),
+    path('student_info', views.student_info, name='student_info'),
     path('status', views.status, name='status'),
 
 ]
